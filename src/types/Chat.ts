03628b524dccbec7898ab1461b540e8343dee1f4
ziv <ziv111ziv@gmail.com>
2025-05-22 19:18:24 +0300

    1.0.22

--- src/types/Chat.ts
+++ src/types/Chat.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
 import { messageSchema, messageSenderSchema } from './message';
+import { extendZod } from '@zodyac/zod-mongoose';
+
+extendZod(z);
 
 export const chatSchema = z.object({
   id: z.string().nonempty(),
